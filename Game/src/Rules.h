9a8ec2d3c9cf2be0094c1f6cd6d9248239934be7
x4iiiis <ryan_o_f@hotmail.co.uk>
2017-11-22 02:09:55 +0000

    :smile:

--- Game/src/Rules.h
+++ Game/src/Rules.h
@@ -20,66 +20,66 @@ public:
 		{
 			//Need to be able to tell who played it, otherwise that black queen is gonna make everyone pick up 5 until we run out of cards
 			//Also it isn't PlayerList[0], it'll be whoever's turn it is once we've got that figured out
-		case(card::type::Queen):
-		{
-			if (lastCard[0]->cardColour == card::colour::Black)
+			case(card::type::Queen):
 			{
-				cout << "The last card was a black queen!" << endl;
-				cout << "You must now pick up 5 cards." << endl << endl;
+				if (lastCard[0]->cardColour == card::colour::Black)
+				{
+					cout << "The last card was a black queen!" << endl;
+					cout << "You must now pick up 5 cards." << endl << endl;
 
-				for (int i = 0; i < 5; i++)
+					for (int i = 0; i < 5; i++)
+					{
+						PlayerList[0]->hand.push_back(cardStack[0]);
+						cardStack.erase(cardStack.begin());
+					}
+					//Display PlayerList[0]'s updated hand
+					cout << PlayerList[0]->name << "'s updated hand:" << endl;
+					//GameManager::Manager()->DeckOfCards.identify_cards(PlayerList[0]->hand);
+				}
+				break;
+			}
+			//Need to be able to tell who played it, otherwise that 2 is gonna make everyone pick up 2 until we run out of cards
+			//Also it isn't PlayerList[0], it'll be whoever's turn it is once we've got that figured out
+			case(card::type::Two):
+			{
+				cout << "The last card was a two!" << endl;
+				cout << "You must now pick 2 cards." << endl << endl;
+
+				for (int i = 0; i < 2; i++)
 				{
 					PlayerList[0]->hand.push_back(cardStack[0]);
 					cardStack.erase(cardStack.begin());
 				}
+
 				//Display PlayerList[0]'s updated hand
 				cout << PlayerList[0]->name << "'s updated hand:" << endl;
 				//GameManager::Manager()->DeckOfCards.identify_cards(PlayerList[0]->hand);
-			}
-			break;
-		}
-		//Need to be able to tell who played it, otherwise that 2 is gonna make everyone pick up 2 until we run out of cards
-		//Also it isn't PlayerList[0], it'll be whoever's turn it is once we've got that figured out
-		case(card::type::Two):
-		{
-			cout << "The last card was a two!" << endl;
-			cout << "You must now pick 2 cards." << endl << endl;
 
-			for (int i = 0; i < 2; i++)
+				break;
+			}
+			//Game management - we need the direction of play to be defined somewhere
+			case(card::type::Jack):
 			{
-				PlayerList[0]->hand.push_back(cardStack[0]);
-				cardStack.erase(cardStack.begin());
+				cout << "The last card was a jack!" << endl;
+				cout << "The direction of play has been reversed." << endl << endl;
+				cout << "*Previous Player*, it's your turn." << endl << endl;
+
+				break;
+			}
+			//Again, needing game management to know whose turn it is and when the card was played, otherwise
+			//everybody's gonna skip their turn forever
+			case(card::type::Eight):
+			{
+				cout << "The last card was an eight!" << endl;
+				cout << "You are forced to skip your turn." << endl << endl;
+
+				cout << "*Next player*, it's your turn." << endl << endl;
 			}
 
-			//Display PlayerList[0]'s updated hand
-			cout << PlayerList[0]->name << "'s updated hand:" << endl;
-			//GameManager::Manager()->DeckOfCards.identify_cards(PlayerList[0]->hand);
-
-			break;
-		}
-		//Game management - we need the direction of play to be defined somewhere
-		case(card::type::Jack):
-		{
-			cout << "The last card was a jack!" << endl;
-			cout << "The direction of play has been reversed." << endl << endl;
-			cout << "*Previous Player*, it's your turn." << endl << endl;
-
-			break;
-		}
-		//Again, needing game management to know whose turn it is and when the card was played, otherwise
-		//everybody's gonna skip their turn forever
-		case(card::type::Eight):
-		{
-			cout << "The last card was an eight!" << endl;
-			cout << "You are forced to skip your turn." << endl << endl;
-
-			cout << "*Next player*, it's your turn." << endl << endl;
-		}
-
-		default:
-		{
-			break;
-		}
+			default:
+			{
+				break;
+			}
 		}
 	}
 };
